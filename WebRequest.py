bf6e8a9f98a70bc8eadab83431abdc1c07a5027a
stinkymonkeyph <monkeydev.team@gmail.com>
2016-10-07 00:36:57 +0800

    fuck the beginning

---
+++ WebRequest.py
@@ -0,0 +1,33 @@
+
+"""
+
+Web Scanner
+
+"""
+
+
+import requests
+
+
+class Crawler:
+
+    def __init__(self):
+        self.url_request = str()
+        self.url_html = str()
+
+    def request_set(self):
+        self.url_request = "http://"+str(input("Enter URL (e.g. www.noobs.com) : "))
+
+    def request_send(self):
+        self.url_html = requests.get(str(self.url_request))
+
+    def request_save(self):
+        file = open("ReconFolder/DotText/SaveWeb.txt", "wb")
+        file.write(self.url_html.text.encode('utf-8').strip())
+        file.close()
+
+
+Crawler = Crawler()
+Crawler.request_set()
+Crawler.request_send()
+Crawler.request_save()
