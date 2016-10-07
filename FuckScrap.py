450fe568206bad5ea0bc3acc001b6d6be2ec58c8
stinkymonkeyph <monkeydev.team@gmail.com>
2016-10-07 20:04:09 +0800

    fix the shit

--- FuckScrap.py
+++ FuckScrap.py
@@ -1,10 +1,21 @@
 import WebCrawler
 
 Crawler = WebCrawler.Crawler()
-Crawler.request_set()
+
+url_request = "http://"+str(input("Enter url to crawl and fuck (e.g. www.noobs.com) : "))
+Crawler.request_set(url_request)
+print("\nFuck Scrap just started crawling ...\n")
+
+print("Sending request ...")
 Crawler.request_send()
 Crawler.request_save()
 Crawler.link_get()
-Crawler.link_enumerate()
-Crawler.recon_save()
 
+print("\n----enumerating anchor tags ...")
+Crawler.link_enumerate()
+print("\n----end enumerating anchor tags\n")
+
+Crawler.recon_save()
+print("------------------------------------\n")
+print("ReconResult save at : ReconFolder/ReconResults/ReconResults.html")
+print("------------------------------------\n")
