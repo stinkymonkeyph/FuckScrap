3fa876beb4ff4c7b28c4ab57be9fedb0194d1b71
stinkymonkeyph <monkeydev.team@gmail.com>
2016-10-15 10:49:02 +0800

    fixed wordpress scanner

--- WordpressDetector.py
+++ WordpressDetector.py
@@ -2,37 +2,62 @@ import requests
 
 
 class WordpressDetect:
+
     def __init__(self, url_request):
         self.is_wordpress_site = False
+        self.has_admin_path = False
+        self.has_login_path = False
+        self.has_changelogs_path = False
+        self.has_xmlrpc_path = False
         self.wordpress_admin = str(url_request) + '/wp-admin'
         self.wordpress_changelogs = str(url_request) + '/changelogs.txt'
         self.wordpress_xmlrpc = str(url_request) + '/xmlrpc.php'
         self.wordpress_login = str(url_request) + '/wp-login.php'
         self.wordpress_install = str(url_request) + '/wp-admin/install.php'
 
-    def check_admin_path(self):
+    def path_admin_check(self):
         url_check = requests.get(self.wordpress_admin)
         if url_check.status_code == 200:
             self.is_wordpress_site = True
+            self.has_admin_path = True
 
-    def check_changelogs_path(self):
+    def path_changelogs_check(self):
         url_check = requests.get(self.wordpress_changelogs)
         if url_check.status_code == 200:
             self.is_wordpress_site = True
+            self.has_changelogs_path = True
 
-    def check_xmlrpc_path(self):
+    def path_xmlrpc_check(self):
         url_check = requests.get(self.wordpress_xmlrpc)
         if url_check.status_code == 200:
             self.is_wordpress_site = True
+            self.has_xmlrpc_path = True
 
-    def check_login_path(self):
+    def path_login_check(self):
         url_check = requests.get(self.wordpress_login)
         if url_check.status_code == 200:
             self.is_wordpress_site = True
+            self.has_login_path = True
+
+    def path_admin_get(self):
+        if self.has_admin_path:
+            return self.wordpress_admin
+
+    def path_login_get(self):
+        if self.has_login_path:
+            return self.wordpress_login
+
+    def path_changelogs_get(self):
+        if self.has_changelogs_path:
+            return self.wordpress_changelogs
+
+    def path_xmlrpc_get(self):
+        if self.has_xmlrpc_path:
+            return self.wordpress_xmlrpc
 
     def is_wordpress(self):
-        self.check_admin_path()
-        self.check_changelogs_path()
-        self.check_xmlrpc_path()
-        self.check_login_path()
+        self.path_admin_check()
+        self.path_changelogs_check()
+        self.path_xmlrpc_check()
+        self.path_login_check()
         return self.is_wordpress_site
